9e4af157059dba5658ccf72c6645c8a6a9342a71
Seyeram Agbasah <agbasahseyeram@gmail.com>
2026-06-01 04:59:56 -0700

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,31 @@
 
+# File: initials.py
+# Description: Prints my initials in large stylized letters.
+# Assignment Number: 2
+#
+# Name: AGBASAH SEYERAM ADZO 
+# STUDENT ID: 2425404080
+# Email: 2425404080@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, AGBASAH SEYERAM ADZO, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
 def main():
-  pass #code goes here
-
-
+    # Print initials in small and large stylized letters
+    print()
+    print("...ASA")
+    print()
+    print("... AAAAAAAAAA.........SSSSSSSSSSSS.........AAAAAAAAAA......")
+    print("...AAA......AAA........SSSSSSSSSSSS........AAA......AAA.....")
+    print("...AAA......AAA........SSS.................AAA......AAA.....")
+    print(".. AAA......AAA........SSS.................AAA......AAA.....")
+    print(".  AAAAAAAAAAAA........SSSSSSSSSSSS........AAAAAAAAAAAA.....")
+    print("...AAAAAAAAAAAA.................SSS........AAAAAAAAAAAA.....")
+    print("...AAA......AAA.................SSS........AAA......AAA.....")
+    print("...AAA......AAA.................SSS........AAA......AAA.....")
+    print("...AAA......AAA...**...SSSSSSSSSSSS...**...AAA......AAA...**")
+    print("...AAA......AAA...**...SSSSSSSSSSSS...**...AAA......AAA...**")
 
 main()
